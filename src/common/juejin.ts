96bdabbadf3364d276b335ad954da66d67605258
Richard Yu <vipzhicheng@gmail.com>
2020-09-03 01:49:10 +0800

    fix: optimize performance

--- src/common/juejin.ts
+++ src/common/juejin.ts
@@ -93,58 +93,60 @@ async function renderPins(topic_id: string, pins: any[], opts) {
 
 {{ pinLink }}
 `
-  const pinsRendered: any[] = []
-  for (let pin of pins) {
+  
 
-    let title = ''
-    if (pin.author_user_info.job_title || pin.author_user_info.company) {
-      if (pin.author_user_info.job_title) {
-        title += pin.author_user_info.job_title
+  for (const chunkedPins of Utils._.chunk(pins, opts.size)) {
+    const pinsRendered: any[] = []
+    for (let pin of chunkedPins) {
+
+      let title = ''
+      if (pin.author_user_info.job_title || pin.author_user_info.company) {
+        if (pin.author_user_info.job_title) {
+          title += pin.author_user_info.job_title
+        }
+
+        if (pin.author_user_info.company) {
+          title += '@' + pin.author_user_info.company
+        }
+
+        if (title) {
+          title = ' [' + title + ']'
+        }
       }
 
-      if (pin.author_user_info.company) {
-        title += '@' + pin.author_user_info.company
+      let images: any = ''
+      if (opts.less || opts.mdcat || !itSupportTerminalImage()) {
+        images = pin.msg_Info.pic_list.map(image => `![](${image})`).join('\n')
+      } else {
+        const imagesData = await Promise.all(pin.msg_Info.pic_list.map(image => (async function (image) {
+          const body = await got(image.replace('.image', '.png')).buffer()
+          return terminalImage.buffer(body, { width: 'auto', height: 'auto' })
+        })(image)))
+
+        images = imagesData.join('\n\n')
       }
 
-      if (title) {
-        title = ' [' + title + ']'
-      }
+
+      pinsRendered.push(Utils._.template(template)({
+        user: `[${pin.author_user_info.user_name}](https://juejin.im/user/${pin.author_user_info.user_id}) [L${pin.author_user_info.level}]${title}`,
+        date: Utils.day(pin.msg_Info.ctime * 1000).format('YYYY-MM-DD HH:mm'),
+        content: pin.msg_Info.content,
+        images,
+        topic: `[${pin.topic.title}]`,
+        pinLink: `[查看原文](https://juejin.im/pin/${pin.msg_id})`,
+        outLink: pin.msg_Info.url ? ('\n\n===> ' + `[${pin.msg_Info.url_title ? pin.msg_Info.url_title : '相关链接'}](${pin.msg_Info.url})` + ' <===' ) : ''
+      }))
     }
 
-    let images: any = ''
-    if (opts.less || opts.mdcat || !itSupportTerminalImage()) {
-      images = pin.msg_Info.pic_list.map(image => `![](${image})`).join('\n')
-    } else {
-      const imagesData = await Promise.all(pin.msg_Info.pic_list.map(image => (async function (image) {
-        const body = await got(image).buffer()
-	      return terminalImage.buffer(body, { width: 'auto', height: 'auto' })
-      })(image)))
-
-      images = imagesData.join('\n\n')
-    }
-
-
-    pinsRendered.push(Utils._.template(template)({
-      user: `[${pin.author_user_info.user_name}](https://juejin.im/user/${pin.author_user_info.user_id}) [L${pin.author_user_info.level}]${title}`,
-      date: Utils.day(pin.msg_Info.ctime * 1000).format('YYYY-MM-DD HH:mm'),
-      content: pin.msg_Info.content,
-      images,
-      topic: `[${pin.topic.title}]`,
-      pinLink: `[查看原文](https://juejin.im/pin/${pin.msg_id})`,
-      outLink: pin.msg_Info.url ? ('\n\n===> ' + `[${pin.msg_Info.url_title ? pin.msg_Info.url_title : '相关链接'}](${pin.msg_Info.url})` + ' <===' ) : ''
-    }))
-  }
-
-  for (const pins of Utils._.chunk(pinsRendered, opts.size)) {
     if (opts.less) {
-      Utils.consoleReader(marked(pins.join('\n\n---\n\n')), {
+      Utils.consoleReader(marked(pinsRendered.join('\n\n---\n\n')), {
         plugin: 'semo-plugin-juejin',
         identifier: topic_id + ''
       })
     } else if (opts.mdcat && Utils.shell.which('mdcat')) {
       Utils.clearConsole()
 
-      const tmpPath = Utils.consoleReader(pins.join('\n\n---\n\n'), {
+      const tmpPath = Utils.consoleReader(pinsRendered.join('\n\n---\n\n'), {
         plugin: 'semo-plugin-juejin',
         identifier: topic_id + '',
         tmpPathOnly: true
@@ -154,7 +156,7 @@ async function renderPins(topic_id: string, pins: any[], opts) {
       Utils.fs.unlinkSync(tmpPath)
 
       console.log()
-      const input = prompt('Continue？[Y/n] [enter to continue, ^C or n+enter to quit]: ', 'Y', {
+      const input = prompt('Continue？[Y/n] [Press enter to continue, ^C or n+enter to quit]: ', 'Y', {
         echo: ''
       })
 
@@ -163,9 +165,9 @@ async function renderPins(topic_id: string, pins: any[], opts) {
     } else {
       Utils.clearConsole()
 
-      console.log(marked(pins.join('\n\n---\n\n')))
+      console.log(marked(pinsRendered.join('\n\n---\n\n')))
 
-      const input = prompt('Continue？[Y/n] [enter to continue, ^C or n+enter to quit]: ', 'Y', {
+      const input = prompt('Continue？[Y/n] [Press enter to continue, ^C or n+enter to quit]: ', 'Y', {
         echo: ''
       })
 
